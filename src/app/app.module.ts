67b75b40acffec0696416a47d7ec3587566e53df
enriqueedelberto <enriqueviiireyes@gmail.com>
2019-08-03 11:49:59 -0500

    Create Home component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Router} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { TaskComponent } from './components/task/task.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+//se importan las rutas
+import { ROUTES } from './app.route';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    TaskComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(ROUTES, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
